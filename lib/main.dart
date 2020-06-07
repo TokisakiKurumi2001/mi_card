0bbb8680546c0be3eb6c2d15fa74310c93db95b2
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2020-06-07 17:30:12 +0700

    Successfully make the Job card

--- lib/main.dart
+++ lib/main.dart
@@ -9,36 +9,84 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                width: 100.0,
-                color: Colors.red,
+              CircleAvatar(
+                backgroundImage: AssetImage('images/eagle.jpg'),
+                radius: 50.0,
+              ),
+              Text(
+                'Tokisaki Kurumi',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Divider(
+                // this is the horizontal line in Flutter
+                indent: 130.0,
+                endIndent: 130.0,
+                color: Colors.white,
               ),
               Container(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Row(
                   children: <Widget>[
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color: Colors.yellow,
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade500,
                     ),
-                    Container(
-                      width: 100.0,
-                      height: 100.0,
-                      color: Colors.green,
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "123456789",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.mail,
+                      color: Colors.teal.shade500
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      "myemail@example.com",
+                      style: TextStyle(
+                        color: Colors.teal.shade900,
+                      ),
                     ),
                   ],
                 )
               ),
-              Container(
-                width: 100.0,
-                color: Colors.blue,
-              ),
             ],
-          ),
+          )
         ),
       ),
     );
