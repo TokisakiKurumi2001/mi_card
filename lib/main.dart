c542bb3024cda5e4f45ea97ad800b8f87f40ade7
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2020-06-10 21:56:05 +0700

    Use the Padding widget to add padding to its child in Card widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,42 +9,43 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child:Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: AssetImage('images/eagle.jpg'),
-                radius: 50.0,
-              ),
-              Text(
-                'Tokisaki Kurumi',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  color: Colors.white,
-                  fontSize: 40.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                "FLUTTER DEVELOPER",
-                style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  color: Colors.teal.shade100,
-                  fontSize: 20.0,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Divider(
-                // this is the horizontal line in Flutter
-                indent: 130.0,
-                endIndent: 130.0,
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              backgroundImage: AssetImage('images/eagle.jpg'),
+              radius: 50.0,
+            ),
+            Text(
+              'Tokisaki Kurumi',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
                 color: Colors.white,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold,
               ),
-              Container(
-                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.teal.shade100,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Divider(
+              // this is the horizontal line in Flutter
+              indent: 130.0,
+              endIndent: 130.0,
+              color: Colors.white,
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              color: Colors.white,
+              child: Padding(
                 padding: EdgeInsets.all(10.0),
-                color: Colors.white,
                 child: Row(
                   children: <Widget>[
                     Icon(
@@ -63,31 +64,29 @@ class MyApp extends StatelessWidget {
                   ],
                 ),
               ),
-              Container(
+            ),
+            Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-                padding: EdgeInsets.all(10.0),
                 color: Colors.white,
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.mail,
-                      color: Colors.teal.shade500
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      "myemail@example.com",
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
+                child: Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.mail, color: Colors.teal.shade500),
+                      SizedBox(
+                        width: 10.0,
                       ),
-                    ),
-                  ],
-                )
-              ),
-            ],
-          )
-        ),
+                      Text(
+                        "myemail@example.com",
+                        style: TextStyle(
+                          color: Colors.teal.shade900,
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+          ],
+        )),
       ),
     );
   }
