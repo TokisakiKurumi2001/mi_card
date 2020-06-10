ef541be6a80a4a21360f13f8dd7fa4c77e54c913
Le Minh Khoi <minhkhoile2001nsg@gmail.com>
2020-06-10 22:04:09 +0700

    Use ListTile to automatically create beautiful card

--- lib/main.dart
+++ lib/main.dart
@@ -9,84 +9,71 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CircleAvatar(
-              backgroundImage: AssetImage('images/eagle.jpg'),
-              radius: 50.0,
-            ),
-            Text(
-              'Tokisaki Kurumi',
-              style: TextStyle(
-                fontFamily: 'Pacifico',
-                color: Colors.white,
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundImage: AssetImage('images/eagle.jpg'),
+                radius: 50.0,
               ),
-            ),
-            Text(
-              "FLUTTER DEVELOPER",
-              style: TextStyle(
-                fontFamily: 'Source Sans Pro',
-                color: Colors.teal.shade100,
-                fontSize: 20.0,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Divider(
-              // this is the horizontal line in Flutter
-              indent: 130.0,
-              endIndent: 130.0,
-              color: Colors.white,
-            ),
-            Card(
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-              color: Colors.white,
-              child: Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal.shade500,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      "123456789",
-                      style: TextStyle(
-                        color: Colors.teal.shade900,
-                      ),
-                    ),
-                  ],
+              Text(
+                'Tokisaki Kurumi',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  color: Colors.white,
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-            ),
-            Card(
+              Text(
+                "FLUTTER DEVELOPER",
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 30.0,
+                width: 200.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
                 color: Colors.white,
-                child: Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: Row(
-                    children: <Widget>[
-                      Icon(Icons.mail, color: Colors.teal.shade500),
-                      SizedBox(
-                        width: 10.0,
-                      ),
-                      Text(
-                        "myemail@example.com",
-                        style: TextStyle(
-                          color: Colors.teal.shade900,
-                        ),
-                      ),
-                    ],
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade500,
                   ),
-                )),
-          ],
-        )),
+                  title: Text(
+                    "123456789",
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                color: Colors.white,
+                child: ListTile(
+                  leading: Icon(Icons.mail, color: Colors.teal.shade500),
+                  title: Text(
+                    "myemail@example.com",
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
